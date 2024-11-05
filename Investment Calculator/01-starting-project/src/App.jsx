6f108338eda58fc32d2d2b8c829a3d1cec4e995b
Markovic Matija <markovicmatija2000@gmail.com>
2024-11-05 16:24:52 +0100

    Input for users and header done

---
+++ Investment Calculator/01-starting-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header.jsx"
+import UserInput from "./components/InputUser.jsx"
+
+function App() {
+  return <>
+    <Header />
+    <UserInput/>
+    </>
+}
+
+export default App
